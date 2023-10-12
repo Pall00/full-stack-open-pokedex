4e240c908215cf2a00fc5a5b26217a0005f10ff5
Pall00 <juhoahol@gmail.com>
2023-10-12 15:03:51 +0300

    fly

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log('server started on port ${PORT}')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
